e4e2fdbac3334946639f96d91b9e210ed85f145e
Rishikesh <rishikeshsinghgautam155@gmail.com>
2026-01-11 12:51:57 +0530

    new exercise added

---
+++ src/Exercise_3.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+public class Exercise_3 {
+    public static void main(String[] args) {
+        System.out.println("Enter your name");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Hey " + name + " Good morning have a nice day");
+
+    }
+}
